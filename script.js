fbfd9815c13f8815984dd6d6323c9c0363c5fb0e
matham1201 <112947368+matham1201@users.noreply.github.com>
2024-04-24 15:22:11 +0200

    Modification de la fonction fetchEvents en filtrant les données voulues

--- script.js
+++ script.js
@@ -2,8 +2,27 @@
 async function fetchEvents() {
     const response = await fetch('https://api.smarkets.com/v3/events/?state=upcoming&type=mma_match&type_domain=mma&type_scope=single_event&sort=id&limit=20');
     const data = await response.json();
-    return data.events;
-  }
+    const filteredEvents = data.events.map(event => ({
+      bet_allowed: event.bet_allowed,
+      bettable: event.bettable,
+      created: event.created,
+      description: event.description,
+      full_slug: event.full_slug,
+      hidden: event.hidden,
+      id: event.id,
+      inplay_enabled: event.inplay_enabled,
+      name: event.name,
+      parent_id: event.parent_id,
+      short_name: event.short_name,
+      slug: event.slug,
+      special_rules: event.special_rules,
+      start_date: event.start_date,
+      start_datetime: event.start_datetime,
+      state: event.state,
+      type: event.type
+    }));
+    return filteredEvents;
+}
 
 async function displayEvents() {
     const events = await fetchEvents();
@@ -14,6 +33,6 @@ async function displayEvents() {
       listItem.textContent = event.name;
       eventsList.appendChild(listItem);
     });
-  }
+}
 
 displayEvents();
